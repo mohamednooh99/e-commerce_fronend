07b859345bf3fa25e5040f7ce3d37ff8f602842b
Mohamed Nooh <noohmohamed1999@gmail.com>
2024-11-28 12:34:12 +0200

    modify title and meta tag

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,7 @@ import Header from "./_component/Header";
 import Footer from "./_component/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { CartContext } from "./_context/CartContext";
-import {  useState } from "react"; 
-
+import { useState } from "react";
 
 const geistSans = Roboto({
   src: "./fonts/GeistVF.woff",
@@ -18,19 +17,24 @@ const geistMono = Roboto({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-}); 
-
+});
 
 export default function RootLayout({ children }) {
   const [openCart, setOpenCart] = useState(false);
-  const [cart, setCart] = useState( [])
-
+  const [cart, setCart] = useState([]);
 
   return (
     <ClerkProvider>
-      <CartContext.Provider value={{ cart, setCart,openCart, setOpenCart }}>
-        <html lang="en"> 
-          <CustomHead />
+      <CartContext.Provider value={{ cart, setCart, openCart, setOpenCart }}>
+        <html lang="en">
+          {/* <CustomHead /> */}
+          <Head>
+            <title>Elessi Website store</title>
+            <meta
+              name="description"
+              content="This is a brief description of the page content, which will appear in search results."
+            />
+          </Head>
           <body
             className={`${geistSans.variable} ${geistMono.variable}   antialiased grid h-screen grid-rows-[auto_1fr_auto]`}
           >
